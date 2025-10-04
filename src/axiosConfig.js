7af6beb92804a7f86aff5795f15cc3c8cc93fe13
Solomon Hunegnaw <hunegnaws@gmail.com>
2025-10-04 08:54:07 +0300

    first commit

---
+++ src/axiosConfig.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+const axiosBase = axios.create({
+  baseURL: "http://localhost:2025/api",
+});
+export default axiosBase;
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { UserProvider } from "./component/Dataprovide/DataProvider.jsx";
+
+const authToken = localStorage.getItem("authtoken");
+const initialUserData = authToken
+  ? { user: undefined, token: authToken }
+  : { user: undefined, token: undefined };
+
+const rootElement = document.getElementById("root");
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <UserProvider value={initialUserData}>
+      <App />
+    </UserProvider>
+  </React.StrictMode>
+);
