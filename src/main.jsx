409c0a1b0054066f247bf67a86c92318fd949fa1
Solomon Hunegnaw <hunegnaws@gmail.com>
2025-10-05 15:27:45 +0300

    main, App and Axio are created

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import { BrowserRouter } from "react-router-dom";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { UserProvider } from "./component/Dataprovide/DataProvider.jsx";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
 
 const authToken = localStorage.getItem("authtoken");
 const initialUserData = authToken
@@ -26,7 +15,9 @@ const rootElement = document.getElementById("root");
 createRoot(rootElement).render(
   <React.StrictMode>
     <UserProvider value={initialUserData}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </UserProvider>
   </React.StrictMode>
 );
