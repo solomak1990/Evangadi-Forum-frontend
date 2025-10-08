8d6505b21db13f162c0dc3543662d0ab5667bec0
yoditfshatsion <yoditfshatsionj@gmail.com>
2025-10-08 06:20:07 +0300

    just correction

--- src/Pages/Questionlist/SingleQuestion.jsx
+++ src/Pages/Questionlist/SingleQuestion.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import axios from "../../axiosConfig";
 import RingLoader from "./RingLoader/RingLoader";
 import Layout from "./Layout/Layout";
 import classes from "./SingleQuestion.module.css";
