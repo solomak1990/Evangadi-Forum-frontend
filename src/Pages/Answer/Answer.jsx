a52b9f48e2257caa5e80eb7b17a3b0b53e3bb7dd
HAB <89307363+habjal@users.noreply.github.com>
2025-10-07 21:57:02 -0400

    first Commit

--- src/Pages/Answer/Answer.jsx
+++ src/Pages/Answer/Answer.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-const Answer = () => {
+import React from "react";
+import profile from "../../Images/User.png";
+const Answer = ({ answer, userName }) => {
   return (
-    <div>Answer</div>
-  )
-}
+    <div>
+      <hr />
+      <div className="d-md-flex align-items-center justify-space-between">
+        <div className="d-flex flex-md-column">
+          <img className="avatar" src={profile} alt="User Avatar" />
+          <h6 className="align-self-center ms-2 ms-md-0">{userName}</h6>
+        </div>
+        <div className="ms-md-5">
+          <h6 className="pt-2 pt-md-0">{answer}</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Answer
+export default Answer;
