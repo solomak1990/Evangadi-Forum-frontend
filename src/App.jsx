a0466ada27b58ab319cf0621a54ce9250a4c7681
Ekhlas61 <ekhlasabdulmelik@gmail.com>
2025-10-09 15:58:12 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,84 @@
+// import React, { useContext, useEffect, useState, createContext } from "react";
+// import { Routes, Route, useNavigate } from "react-router-dom";
+// import { UserContext } from "./component/Dataprovider/DataProvider.jsx";
+// import Home from "./Pages/Home/Home.jsx";
+// import Login from "./Pages/Login/Login.jsx";
+// import axios from "./axiosConfig";
+// import Question from "./Pages/Question/Question.jsx"
+// import Register from "./Pages/Register/Register.jsx"
+// import Answer from "./Pages/Answer/Answer.jsx";
+//  import axiosBase from "./axiosConfig";
+// import Profile from "./component/Header/Profile";
+// import NotFound from "./Pages/Login/Notfound";
+// import QuestionList from "./Pages/QuestionList/QuestionList"
+
+// // import Profile from "./component/Header/Profile";
+// // import NotFound from "./pages/login/Notfound";
+// export const AppState = createContext();
+// function App() {
+//   const [userData, setUserData] = useContext(UserContext);
+//   let token = localStorage.getItem("token");
+//   const navigate = useNavigate();
+//   const [user, setUser] = useState();
+//   const checkUser2 = async () => {
+//     try {
+//       const { data } = await axios.get("api/users/checkUser", {
+//         headers: {
+//           Authorization: "Bearer " + token,
+//         },
+//       });
+//       setUserData({ user: data, token: token });
+//       setUser(data);
+//       console.log(data);
+//     } catch (error) {
+//       console.log(error);
+//       navigate("/login");
+//     }
+//   };
+
+  
+//   useEffect(() => {
+
+//   if(token){
+//   checkUser2();
+//   }
+//   else{
+//     navigate("/login");
+//   }
+    
+//   }, []);
+//   return (
+//     <AppState.Provider value={{ user, setUser }}>
+//       <Routes>
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<Register />} />
+//         <Route path="/" element={<Home />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/question" element={<Question />} />
+//         <Route path="/question/:id" element={<Answer />} />
+//         <Route path="/allquestion" element={<QuestionList />} />
+//         <Route path="/profile" element={<Profile />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </AppState.Provider>
+//   );
+// }
+// export default App;
+
+
+
+
 import React, { useContext, useEffect, useState, createContext } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { UserContext } from "./component/Dataprovider/DataProvider.jsx";
 import Home from "./Pages/Home/Home.jsx";
 import Login from "./Pages/Login/Login.jsx";
 import axios from "./axiosConfig";
-import Question from "./Pages/Question/Question.jsx"
-import Register from "./Pages/Register/Register.jsx"
+import Question from "./Pages/Question/Question.jsx";
+import Register from "./Pages/Register/Register.jsx";
 import Answer from "./Pages/Answer/Answer.jsx";
- import axiosBase from "./axiosConfig";
-import Profile from "./component/Header/Profile";
 import NotFound from "./Pages/Login/Notfound";
-import QuestionList from "./Pages/QuestionList/QuestionList"
+import QuestionList from "./Pages/QuestionList/QuestionList";
 
 // import Profile from "./component/Header/Profile";
 // import NotFound from "./pages/login/Notfound";
@@ -22,7 +90,7 @@ function App() {
   const [user, setUser] = useState();
   const checkUser2 = async () => {
     try {
-      const { data } = await axios.get("api/users/checkUser", {
+      const { data } = await axios.get("api/user/checkUser", {
         headers: {
           Authorization: "Bearer " + token,
         },
@@ -31,21 +99,17 @@ function App() {
       setUser(data);
       console.log(data);
     } catch (error) {
-      console.log(error);
+      // console.log(error);
       navigate("/login");
     }
   };
 
-  
   useEffect(() => {
-
-  if(token){
-  checkUser2();
-  }
-  else{
-    navigate("/login");
-  }
-    
+    if (token) {
+      checkUser2();
+    } else {
+      navigate("/login");
+    }
   }, []);
   return (
     <AppState.Provider value={{ user, setUser }}>
@@ -57,14 +121,9 @@ function App() {
         <Route path="/question" element={<Question />} />
         <Route path="/question/:id" element={<Answer />} />
         <Route path="/allquestion" element={<QuestionList />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </AppState.Provider>
   );
 }
 export default App;
-
-
-
-
