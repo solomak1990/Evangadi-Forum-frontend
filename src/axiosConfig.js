d1461157f61cd27bb709faf096940fd6f696c442
Ekhlas61 <ekhlasabdulmelik@gmail.com>
2025-10-09 07:51:21 +0300

    axios updated

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosBase = axios.create({
   // baseURL: "http://localhost:2025/api",
-  baseURL: "http://forumevangadi.natesirak.com/",
+  baseURL: "http://forumevangadi.natesirak.com/api",
 });
 export default axiosBase;
 
